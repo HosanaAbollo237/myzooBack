fd7ae592d890691751edf759dc17bb3ae65a67cc
HosanaAbollo237 <hosana.abollo237@gmail.com>
2021-05-20 16:01:20 +0200

    retrieve api data - bug useEffect..

--- react_myzoo/src/containers/Parc/Parc.js
+++ react_myzoo/src/containers/Parc/Parc.js
@@ -1,23 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import Title from '../../components/UI/Title/Title'
 import axios from 'axios'
 
 class Parc extends Component{
   state = {
-    animaux: null
+    animals: {}
+  }
+  
+   datas = null;
+
+  componentDidMount = () => {
+      this.setApiState(this.state)
+      console.log("VERIFIER LE STATE AVEC DEV TOOLS")
   }
 
-  getAnimals = () => {
+
+  setApiState = (aState, aProps) => {
+    const newState = {...aState}
     axios.get('http://localhost/myzoo/front/animals')
-      .then(response =>( console.log(response)))
+    .then(response => {
+       aState = response.data
+       this.setState({
+        animals: aState
+       })
+    })
   }
-  render() {
 
+  render() {
     return (
       <>
         <Title>{this.props.title}</Title>
         <div>
             Page listant les animaux du zoo
+        </div>
+        <div>
         </div>
       </>
     );
